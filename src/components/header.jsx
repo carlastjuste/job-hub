6488757a46326afb26b70e26a4826332f51aec51
carlastjuste <carlastjuste@users.noreply.github.com>
2021-04-03 09:56:05 -0400

    add script in index.html

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,27 +1,15 @@
 import React, { Component } from 'react';
-import ReactDOM from "react-dom";
+
 
 
 class Header extends Component {
     state = {  }
 
-    componentDidMount() {
-        const script = document.createElement("script");
-        script.async = true;
-        script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
-        this.div.appendChild(script);
-
-        const script1 = document.createElement("script");
-        script1.async = true;
-        script1.src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js";
-        this.div.appendChild(script1);
-      }
-
 
     render() { 
         return ( 
     <React.Fragment>
-      <div className="Header" ref={el => (this.div = el)}>
+      <div className="Header">
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="navbar-collapse" id="navbarNav">
                 <div className="navbar-header">
@@ -29,8 +17,8 @@ class Header extends Component {
                 </div>
                 <ul className="nav navbar-nav justify-content-center">
                     <li className="active"><a href="/hi">Job Application</a></li>
-                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="/testPage1">Page 1 <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
+                    <li className="dropdown"><a className="dropdown-toggle" data-toggle="dropdown" href="/testPage1">Page 1 <span className="caret"></span></a>
+                        <ul className="dropdown-menu">
                             <li><a href="#">Page 1-1</a></li>
                             <li><a href="#">Page 1-2</a></li>
                             <li><a href="#">Page 1-3</a></li>
@@ -38,9 +26,9 @@ class Header extends Component {
                     </li>
                     <li><a href="/test">Page 2</a></li>
                 </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="/registration"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                    <li><a href="/login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                <ul className="nav navbar-nav navbar-right">
+                    <li><a href="/registration"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a href="/login"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
                 </ul>
                 </div>
                 </nav>
