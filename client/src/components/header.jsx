a98a01c315196cece442a297b8d7741ea0b70d80
carlastjuste <carlastjuste@users.noreply.github.com>
2021-04-03 15:08:41 -0400

    Update NavBar

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -22,7 +22,7 @@ class Header extends Component {
                 <ul className="nav navbar-nav justify-content-center">
 
                     <li className="active"><a href="/job">My Jobs</a></li>
-                    <li className="dropdown"><a className="dropdown-toggle" data-toggle="dropdown" href="/testPage1">Page1<span className="caret"></span></a>
+                    <li className="dropdown"><a className="dropdown-toggle" data-toggle="dropdown" href="/testPage1">Resume<span className="caret"></span></a>
 
 
                         <ul className="dropdown-menu">
@@ -31,7 +31,7 @@ class Header extends Component {
                             <li><a href="#">Page 1-3</a></li>
                         </ul>
                     </li>
-                    <li><a href="/test" className="nav-item px-3">Page 2</a></li>
+                    <li><a href="/test" className="nav-item px-3">Profile</a></li>
                 </ul>
                 {/* <ul className="nav navbar-nav navbar-right"> */}
                 <ul class="navbar-nav ml-auto">
