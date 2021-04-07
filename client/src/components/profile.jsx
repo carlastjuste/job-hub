b2a1d61fb42ea8c3aec2bc9342bacb35d31dca2b
carlastjuste <carlastjuste@users.noreply.github.com>
2021-04-07 01:17:53 -0400

    parse html to react

--- client/src/components/profile.jsx
+++ client/src/components/profile.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 import API from "../utils/API";
 import _ from 'lodash';
 
@@ -68,16 +69,16 @@ render() {
             <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong"> View More</button>
 
             <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-                <div class="modal-dialog" role="document">
+                <div class="modal-dialog modal-lg" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                            <h5 class="modal-title" id="exampleModalLongTitle">{job.name}<p class="font-weight-light">{job.company.name}</p></h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close"> 
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <div class="modal-body">
-                            {job.contents}
+                           {ReactHtmlParser(job.contents)}
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
