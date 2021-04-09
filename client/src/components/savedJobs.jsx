8eca915a0bcb48da5c36017f66086fccb30829e4
carlastjuste <carlastjuste@users.noreply.github.com>
2021-04-08 21:52:36 -0400

    Update jsx in savedJob component

--- client/src/components/savedJobs.jsx
+++ client/src/components/savedJobs.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import axios from 'axios';
-
 const apiEndPoint = "http://localhost:5000/jobs/";
 
 
@@ -20,18 +19,25 @@ class SavedJobs extends Component {
     render() { 
         return (
 <React.Fragment>
+    <div className ="card-deck"> 
     {this.state.jobs.map((job, index) => (
     <div className="card" >
         <div className="card-header">
-            {job.jobtitle}
+            <b>{job.jobtitle}</b>
         </div>
     <ul className="list-group list-group-flush">
-        <li className="list-group-item">Cras justo odio</li>
-        <li className="list-group-item">Dapibus ac facilisis in</li>
-        <li className="list-group-item">Vestibulum at eros</li>
+        <li className="list-group-item" key={index + 1}>Company: <i>{job.compagny}</i></li>
+        <li className="list-group-item" key={index + 2}>Location: <i>{job.city}, {job.state} - {job.country}</i></li>
+        <li className="list-group-item" key={index + 3}>Status: <i>{job.status}</i></li>
+        <li className="list-group-item" key={index + 4}>Note: <i>{job.note}</i></li>
     </ul>
+    <div class="card-body">
+    <a href={job.link} className="card-link">Job link</a>
+    <a href="#" className="card-link">Another link</a>
+  </div>
     </div>
     ))}
+    </div>
 </React.Fragment>
          );
     }
