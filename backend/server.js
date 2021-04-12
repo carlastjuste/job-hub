6240b3b0bbdb4caa5d82b7e25affa82cc1afdfab
carlastjuste <carlastjuste@users.noreply.github.com>
2021-04-11 21:41:41 -0400

    Change for heroku issue

--- backend/server.js
+++ backend/server.js
@@ -34,10 +34,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  // const path = require('path');
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // });
 }
 
 
