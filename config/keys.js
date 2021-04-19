e6d249c144a607d3d47de994be6fd00739b266d7
Carla ST JUSTE <carlastjuste@gmail.com>
2021-04-19 17:51:06 -0400

    testing

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,12 @@
-// keys.js - figure out what set of credentials to return
-if (process.env.NODE_ENV === 'production') {
-    // we are in production - return the prod set of keys
-    module.exports = require('./prod');
-  } else {
-    // we are in development - return the dev keys!!!
-    module.exports = require('./dev');
-  }
-  
+// // keys.js - figure out what set of credentials to return
+// if (process.env.NODE_ENV === 'production') {
+//     // we are in production - return the prod set of keys
+//     module.exports = require('./prod');
+//   } else {
+//     // we are in development - return the dev keys!!!
+//     module.exports = require('./dev');
+//   }
+module.exports = {
+    googleClientId: '14211568399-hdiuunagoequs0014nro7vmuh0k7ik67.apps.googleusercontent.com',
+    googleClientSecret: 'ZEZkf-BSpxHSxLDUbFsR5ciY'
+   }
